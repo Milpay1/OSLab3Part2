faa20edbd5b0bb1a55606b9f3142cdd76c9783c3
Miles Muray <miles.murray@bison.howard.edu>
2024-11-03 21:06:38 +0000

    rules and task implemented

--- shm_processes.c
+++ shm_processes.c
@@ -1,71 +1,116 @@
-#include  <stdio.h>
-#include  <stdlib.h>
-#include  <sys/types.h>
-#include  <sys/ipc.h>
-#include  <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <stdint.h>  // Include for intptr_t
 
-void  ClientProcess(int []);
+#define NUM_ITERATIONS 25
 
-int  main(int  argc, char *argv[])
-{
-     int    ShmID;
-     int    *ShmPTR;
-     pid_t  pid;
-     int    status;
+void ParentProcess(int *sharedMemory);
+void ChildProcess(int *sharedMemory);
 
-     if (argc != 5) {
-          printf("Use: %s #1 #2 #3 #4\n", argv[0]);
-          exit(1);
-     }
+int main() {
+    int ShmID;
+    int *ShmPTR;
+    pid_t pid;
+    int status;
 
-     ShmID = shmget(IPC_PRIVATE, 4*sizeof(int), IPC_CREAT | 0666);
-     if (ShmID < 0) {
-          printf("*** shmget error (server) ***\n");
-          exit(1);
-     }
-     printf("Server has received a shared memory of four integers...\n");
+    // Create shared memory for BankAccount and Turn
+    ShmID = shmget(IPC_PRIVATE, 2 * sizeof(int), IPC_CREAT | 0666);
+    if (ShmID < 0) {
+        printf("*** shmget error (server) ***\n");
+        exit(1);
+    }
 
-     ShmPTR = (int *) shmat(ShmID, NULL, 0);
-     if (*ShmPTR == -1) {
-          printf("*** shmat error (server) ***\n");
-          exit(1);
-     }
-     printf("Server has attached the shared memory...\n");
+    ShmPTR = (int *)shmat(ShmID, NULL, 0);
+    if ((intptr_t)ShmPTR == -1) {  // Changed to intptr_t to avoid casting warning
+        printf("*** shmat error (server) ***\n");
+        exit(1);
+    }
 
-     ShmPTR[0] = atoi(argv[1]);
-     ShmPTR[1] = atoi(argv[2]);
-     ShmPTR[2] = atoi(argv[3]);
-     ShmPTR[3] = atoi(argv[4]);
-     printf("Server has filled %d %d %d %d in shared memory...\n",
-            ShmPTR[0], ShmPTR[1], ShmPTR[2], ShmPTR[3]);
+    // Initialize shared memory values
+    ShmPTR[0] = 0; // BankAccount
+    ShmPTR[1] = 0; // Turn
 
-     printf("Server is about to fork a child process...\n");
-     pid = fork();
-     if (pid < 0) {
-          printf("*** fork error (server) ***\n");
-          exit(1);
-     }
-     else if (pid == 0) {
-          ClientProcess(ShmPTR);
-          exit(0);
-     }
+    pid = fork();
+    if (pid < 0) {
+        printf("*** fork error (server) ***\n");
+        exit(1);
+    } else if (pid == 0) {
+        ChildProcess(ShmPTR);
+        exit(0);
+    } else {
+        ParentProcess(ShmPTR);
+    }
 
-     wait(&status);
-     printf("Server has detected the completion of its child...\n");
-     shmdt((void *) ShmPTR);
-     printf("Server has detached its shared memory...\n");
-     shmctl(ShmID, IPC_RMID, NULL);
-     printf("Server has removed its shared memory...\n");
-     printf("Server exits...\n");
-     exit(0);
+    wait(&status);
+
+    // Cleanup shared memory
+    shmdt((void *)ShmPTR);
+    shmctl(ShmID, IPC_RMID, NULL);
+
+    printf("Server exits...\n");
+    exit(0);
 }
 
-void  ClientProcess(int  SharedMem[])
-{
-     printf("   Client process started\n");
-     printf("   Client found %d %d %d %d in shared memory\n",
-                SharedMem[0], SharedMem[1], SharedMem[2], SharedMem[3]);
-     printf("   Client is about to exit\n");
+void ParentProcess(int *sharedMemory) {
+    int i, account, balance;
+
+    srand(time(NULL));
+
+    for (i = 0; i < NUM_ITERATIONS; i++) {
+        sleep(rand() % 6);
+
+        account = sharedMemory[0];
+
+        // Wait for Turn to be 0
+        while (sharedMemory[1] != 0);
+
+        if (account <= 100) {
+            balance = rand() % 101;
+            if (balance % 2 == 0) {
+                account += balance;
+                printf("Dear old Dad: Deposits $%d / Balance = $%d\n", balance, account);
+            } else {
+                printf("Dear old Dad: Doesn't have any money to give\n");
+            }
+        } else {
+            printf("Dear old Dad: Thinks Student has enough Cash ($%d)\n", account);
+        }
+
+        sharedMemory[0] = account;
+        sharedMemory[1] = 1; // Set Turn to 1 for Child
+    }
+}
+
+void ChildProcess(int *sharedMemory) {
+    int i, account, balance;
+
+    srand(time(NULL) + 1); // Different seed for child process
+
+    for (i = 0; i < NUM_ITERATIONS; i++) {
+        sleep(rand() % 6);
+
+        account = sharedMemory[0];
+
+        // Wait for Turn to be 1
+        while (sharedMemory[1] != 1);
+
+        balance = rand() % 51;
+        printf("Poor Student needs $%d\n", balance);
+
+        if (balance <= account) {
+            account -= balance;
+            printf("Poor Student: Withdraws $%d / Balance = $%d\n", balance, account);
+        } else {
+            printf("Poor Student: Not Enough Cash ($%d)\n", account);
+        }
+
+        sharedMemory[0] = account;
+        sharedMemory[1] = 0; // Set Turn to 0 for Parent
+    }
 }
